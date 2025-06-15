090c5c04fe892dd7b4c4f636af35a9154128cca1
Gabriel Santana Barroso <g.santana.b@outlook.com>
2025-06-15 19:59:09 -0300

    Ajuste de performance: adiciona lazy loading (Compra)

--- src/main/java/br/com/digio/api/entity/Compra.java
+++ src/main/java/br/com/digio/api/entity/Compra.java
@@ -2,6 +2,7 @@ package br.com.digio.api.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,11 +20,11 @@ public class Compra {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "produto_codigo")
     private Produto produto;
 
